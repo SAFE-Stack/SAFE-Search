6813e8396192715eb4db2796910965f3133357b1
Isaac Abraham <isaac.abraham@gmail.com>
2017-12-28 10:51:40 +0100

    Break Filter into own page.

---
+++ src/client/pages/Filter.fs
@@ -0,0 +1,39 @@
+module Pages.Filter
+open Fable.Helpers.React
+open Fable.Helpers.React.Props
+open PropertyMapper.Contracts
+
+let toFilterCard dispatch facet values =
+    match values with
+    | [] -> div [] []
+    | values ->
+        div [ ClassName "card" ] [
+            div [ ClassName "card-header"; Role "tab"; Id (sprintf "heading-%s" facet) ] [
+                h5 [ ClassName "mb-0" ] [
+                    a [ DataToggle "collapse"; Href (sprintf "#collapse-%s" facet); AriaExpanded true ] [ str facet ]
+                ]
+            ]
+            div [ Id (sprintf "collapse-%s" facet); Role "tabpanel"; ClassName "collapse show" ] [
+                div [ ClassName "card-body" ] [
+                    div [ ClassName "list-group" ]
+                        [ for value in values ->
+                            a [ ClassName ("list-group-item list-group-item-action")
+                                OnClick (fun _ -> dispatch (facet, value))
+                                Href "#" ]
+                              [ str value ] ]
+                ]
+            ]
+        ]
+
+let createFilters dispatch facets =
+    div [ ClassName "container" ] [
+        div [ ClassName "row" ] [ h4 [] [ str "Filters" ] ]
+        div [ ClassName "row" ] [
+            div [ Id "accordion"; Role "tablist" ] [
+                toFilterCard dispatch "Counties" facets.Counties
+                toFilterCard dispatch "Towns" facets.Towns
+                toFilterCard dispatch "Districts" facets.Districts
+                toFilterCard dispatch "Localities" facets.Localities
+            ]
+        ]
+    ]
--- src/client/pages/Results.fs
+++ src/client/pages/Results.fs
@@ -13,28 +13,6 @@ let init _ : Model = { SearchTerm = None; Response = None }
 let view model dispatch =
     let toTh c = th [ Scope "col" ] [ str c ]
     let toTd c = td [ Scope "row" ] [ str c ]
-
-    let toFilterCard dispatch facet values =
-        match values with
-        | [] -> div [] []
-        | values ->
-            div [ ClassName "card" ] [
-                div [ ClassName "card-header"; Role "tab"; Id (sprintf "heading-%s" facet) ] [
-                    h5 [ ClassName "mb-0" ] [
-                        a [ DataToggle "collapse"; Href (sprintf "#collapse-%s" facet); AriaExpanded true ] [ str facet ]
-                    ]
-                ]
-                div [ Id (sprintf "collapse-%s" facet); Role "tabpanel"; ClassName "collapse show" ] [
-                    div [ ClassName "card-body" ] [
-                        div [ ClassName "list-group" ]
-                            [ for value in values ->
-                                a [ ClassName ("list-group-item list-group-item-action")
-                                    OnClick (fun _ -> dispatch (FilterSet(facet, value)))
-                                    Href "#" ]
-                                  [ str value ] ]
-                    ]
-                ]
-            ]
     
     div [ ClassName "container-fluid border rounded m-3 p-3 bg-light" ] [
         match model with
@@ -43,19 +21,7 @@ let view model dispatch =
         | { SearchTerm = Some (SearchTerm text); Response = Some response } ->
             let hits = response.TotalTransactions |> Option.map (sprintf "(%d hits).") |> Option.defaultValue ""
             yield div [ ClassName "row" ] [
-                div [ ClassName "col-2" ] [
-                    div [ ClassName "container" ] [
-                        div [ ClassName "row" ] [ h4 [] [ str "Filters" ] ]
-                        div [ ClassName "row" ] [
-                            div [ Id "accordion"; Role "tablist" ] [
-                                toFilterCard dispatch "Counties" response.Facets.Counties
-                                toFilterCard dispatch "Towns" response.Facets.Towns
-                                toFilterCard dispatch "Districts" response.Facets.Districts
-                                toFilterCard dispatch "Localities" response.Facets.Localities
-                            ]
-                        ]
-                    ]
-                ]
+                div [ ClassName "col-2" ] [ Pages.Filter.createFilters (FilterSet >> dispatch) response.Facets ]
                 div [ ClassName "col-10" ] [
                     div [ ClassName "row" ] [ div [ ClassName "col" ] [ h4 [] [ str <| sprintf "Search results for '%s' %s." text hits ] ] ]
                     table [ ClassName "table table-bordered table-hover" ] [
