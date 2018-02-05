d10a7466961b8a2d9513451698f449a3b9ac7c03
Prashant Pathak <theprash@gmail.com>
2018-02-05 15:15:15 +0000

    Fix property details modal

--- src/client/pages/Details.fs
+++ src/client/pages/Details.fs
@@ -5,7 +5,7 @@ open Fable.Helpers.React.Props
 open PropertyMapper.Contracts
 open Fable
 
-let view txn =
+let private content txn =
     let field name value =
         value
         |> Option.map(fun v ->
@@ -15,31 +15,32 @@ let view txn =
             ])
         |> Option.defaultValue (div [] [])
 
-    div [ ClassName "modal fade"; Id "exampleModal"; Role "dialog"; unbox ("aria-labelledby", "exampleModalLabel"); unbox ("aria-hidden", "true") ] [
-        div [ ClassName "modal-dialog"; Role "document" ] [
-            div [ ClassName "modal-content" ] [
-                div [ ClassName "modal-header" ] [
-                    h5 [ ClassName "modal-title"; Id "exampleModalLabel" ] [ str txn.Address.FirstLine ]
-                    button [ Type "button"; ClassName "close"; unbox ("data-dismiss", "modal"); unbox ("aria-label", "Close") ] [
-                        span [ unbox ("aria-hidden", true) ] [ str "x" ]
-                    ]
-                ]
-                div [ ClassName "modal-body" ] [
-                    field "Building / Street" (Some txn.Address.FirstLine)
-                    field "Town" (Some txn.Address.TownCity)
-                    field "District" (Some txn.Address.District)
-                    field "County" (Some txn.Address.County)
-                    field "Locality" txn.Address.Locality
-                    field "Post Code" txn.Address.PostCode
-                    field "Price" (Some(sprintf "£%s" (txn.Price |> commaSeparate)))
-                    field "Date of Transfer" (Some (txn.DateOfTransfer.ToShortDateString()))
-                    field "Property Type" (txn.BuildDetails.PropertyType |> Option.map(fun pt -> pt.Description))
-                    field "Build Type" (Some (string txn.BuildDetails.Build.Description))
-                    field "Contract" (Some (string txn.BuildDetails.Contract.Description))
-                ]
-                div [ ClassName "modal-footer" ] [
-                    button [ Type "button"; ClassName "btn btn-primary"; unbox ("data-dismiss", "modal") ] [ str "Close" ]
-                ]
+    div [ ClassName "modal-content" ] [
+        div [ ClassName "modal-header" ] [
+            h5 [ ClassName "modal-title"; Id "exampleModalLabel" ] [ str txn.Address.FirstLine ]
+            button [ Type "button"; ClassName "close"; unbox ("data-dismiss", "modal"); unbox ("aria-label", "Close") ] [
+                span [ unbox ("aria-hidden", true) ] [ str "x" ]
             ]
         ]
+        div [ ClassName "modal-body" ] [
+            field "Building / Street" (Some txn.Address.FirstLine)
+            field "Town" (Some txn.Address.TownCity)
+            field "District" (Some txn.Address.District)
+            field "County" (Some txn.Address.County)
+            field "Locality" txn.Address.Locality
+            field "Post Code" txn.Address.PostCode
+            field "Price" (Some(sprintf "£%s" (txn.Price |> commaSeparate)))
+            field "Date of Transfer" (Some (txn.DateOfTransfer.ToShortDateString()))
+            field "Property Type" (txn.BuildDetails.PropertyType |> Option.map(fun pt -> pt.Description))
+            field "Build Type" (Some (string txn.BuildDetails.Build.Description))
+            field "Contract" (Some (string txn.BuildDetails.Contract.Description))
+        ]
+        div [ ClassName "modal-footer" ] [
+            button [ Type "button"; ClassName "btn btn-primary"; unbox ("data-dismiss", "modal") ] [ str "Close" ]
+        ]
+    ]
+
+let view txn =
+    div [ ClassName "modal fade"; Id "exampleModal"; Role "dialog"; unbox ("aria-labelledby", "exampleModalLabel"); unbox ("aria-hidden", "true") ] [
+        div [ ClassName "modal-dialog"; Role "document" ] (match txn with Some txn -> [ content txn ] | None -> [])
     ]
