f306047f3a6c57af673ce932612911652aa62963
Isaac Abraham <isaac.abraham@gmail.com>
2018-02-06 20:23:21 +0100

    Integrate Saturn.

--- src/server/Routing.fs
+++ src/server/Routing.fs
@@ -1,34 +1,30 @@
 module PropertyMapper.Routing
 
-open PropertyMapper.Search
-open PropertyMapper.Contracts
-open Giraffe
-open Microsoft.AspNetCore.Http
-
-let searchProperties (searcher:Search.ISearch) (postCode:string, distance, page) next (ctx:HttpContext) = task {
-    let! properties =
-        searcher.PostcodeSearch
-            { Filter = ctx.BindQueryString<PropertyFilter>()
-              Postcode = postCode.ToUpper()
-              MaxDistance = distance
-              Page = page }
-    return! FableJson.serialize properties next ctx }
-
-let genericSearch (searcher:Search.ISearch) (text, page) next (ctx:HttpContext) =
-    let request =
-        { Page = page
-          Text = if System.String.IsNullOrWhiteSpace text then None else Some text
-          Filter = ctx.BindQueryString<PropertyFilter>() }
-    task {
-        let! properties = searcher.GenericSearch request
+module Adapters =
+    open Microsoft.AspNetCore.Http
+    open Giraffe
+    open PropertyMapper.Search
+    open PropertyMapper.Contracts
+    let searchProperties (searcher:Search.ISearchEngine) (postCode:string, distance, page) next (ctx:HttpContext) = task {
+        let! properties =
+            searcher.PostcodeSearch
+                { Filter = ctx.BindQueryString<PropertyFilter>()
+                  Postcode = postCode.ToUpper()
+                  MaxDistance = distance
+                  Page = page }
         return! FableJson.serialize properties next ctx }
-let webApp searcher : HttpHandler =
-    choose [
-        GET >=>
-            choose [
-                routef "/property/find/%s/%i" (genericSearch searcher)
-                routef "/property/%s/%i/%i" (searchProperties searcher)
-                routef "/property/%s/%i" (fun (postcode, distance) -> searchProperties searcher (postcode, distance, 0))
-            ]
-        setStatusCode 404 >=> text "Not Found" ]
 
+    let genericSearch (searcher:Search.ISearchEngine) (text, page) next (ctx:HttpContext) = task {
+        let! properties =
+            let request =
+                { Page = page
+                  Text = if System.String.IsNullOrWhiteSpace text then None else Some text
+                  Filter = ctx.BindQueryString<PropertyFilter>() }
+            searcher.GenericSearch request
+        return! FableJson.serialize properties next ctx }
+
+open Saturn.Router
+let propertyRouter searcher = scope {
+    getf "/find/%s/%i" (Adapters.genericSearch searcher)
+    getf "/%s/%i/%i" (Adapters.searchProperties searcher)
+    getf "/%s/%i" (fun (postcode, distance) -> Adapters.searchProperties searcher (postcode, distance, 0)) }
